73633c75a8be6a88d410214e21f5da5ab11e34a2
chenglong <chenglong86@huawei.com>
2025-07-26 15:40:04 +0800

    [Agent] Add agent - G2CTransAgent to translate CUDA(GPU) to CPP(CPU)

---
+++ G2CTransAgent/CUDA_C_Agent.py
@@ -0,0 +1,41 @@
+import json
+import Langchain_LLM as LL
+import os
+import Global_vars as GV
+
+def readdata(filename):
+    data_list = []
+    with open(filename, 'r', encoding='utf-8') as f:
+        data_list = [json.loads(line) for line in f]
+    return data_list
+#
+# features: ['id', 'cpp_code', 'cuda_code', 'consistent_cpp_inputs', 'consistent_cuda_inputs', 'cuda_wrapper', 'consistent_outputs'], num_rows: 233
+filename = "BabelTower/test/mono_cases.jsonl"
+C_CUDA_Data = readdata(filename)
+#
+agent = LL.TriTransformAgent()
+#import pdb
+#pdb.set_trace()
+num_rows = len(C_CUDA_Data)
+#
+for i in range(20, num_rows, 1):
+    print(f"===================id: {i}====================")
+    filename = "./CUDA_C_Results/" + str(i) + "_main.cpp"
+    GV.ABS_SAVE_PATH = "./CUDA_C_Results/"
+    GV.FILE_INDEX = i
+    #
+    test_code = C_CUDA_Data[i]["cuda_code"]
+    ref_code = C_CUDA_Data[i]["cpp_code"]
+    
+    try:
+        optimized_code = agent.run(test_code)
+        print(f"Original Code: \n{test_code} \n   \nOptimized Code：\n{optimized_code}")
+        
+        if os.path.exists(filename) and os.path.isfile(filename):
+            os.remove(filename)
+        with open(filename, "a") as f:
+            f.write(f"// original code(id = {i}): \n /*\n{test_code}\n*/\n")
+            f.write(f"// optimized code: \n\n")
+            f.write(optimized_code)
+    except:
+        print(f"LLM inference failed, jump to the next task: i = {i+1}")
